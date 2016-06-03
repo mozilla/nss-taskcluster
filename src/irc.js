85b57f9d176806acc0598683335f6b16ea248946
Tim Taubert <tim@timtaubert.de>
2016-06-03 19:05:58 +0200

    post links and details to bug numbers on irc, like firebot

--- src/irc.js
+++ src/irc.js
@@ -3,12 +3,28 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import irc from "irc";
+import request from "request-json";
 
 const NICK = "nss-tc";
 const CHANNEL = "#nss";
 const SERVER = "irc.mozilla.org";
+const BZ_HOST = "https://bugzilla.mozilla.org";
 
 let client;
+
+async function jsonRequest(url) {
+  let client = request.createClient(BZ_HOST);
+
+  return new Promise((resolve, reject) => {
+    client.get(url, (err, res, json) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(json);
+      }
+    });
+  });
+}
 
 function say(msg) {
   if (!client) {
@@ -33,6 +49,21 @@ function connect() {
   // Keep it from crashing.
   client.addListener("error", msg => {
     console.log("irc error: ", msg);
+  });
+
+  // Listen for bug numbers.
+  client.addListener(`message${CHANNEL}`, async function (from, message) {
+    let match = /[Bb]ug (\d{5,})/.exec(message);
+    if (!match) {
+      return;
+    }
+
+    let id = match[1];
+    let client = request.createClient(BZ_HOST);
+    let {bugs: [bug]} = await jsonRequest(`/rest/bug/${id}`);
+    let status = bug.resolution || bug.status;
+
+    say(`https://bugzil.la/${id} — ${status}, ${bug.assigned_to} — ${bug.summary}`);
   });
 }
 
